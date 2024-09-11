6cf23854ad6035ff9151dec853d48919fdb78a75
William Wun <williamwun@hotmail.com>
2024-09-10 17:31:24 -0700

    project started

---
+++ src/main/java/com/william/springboot/msvc/products/msvc_products/MsvcProductsApplication.java
@@ -0,0 +1,13 @@
+package com.william.springboot.msvc.products.msvc_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/springboot/msvc/products/msvc_products/MsvcProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.springboot.msvc.products.msvc_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
